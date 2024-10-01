72f894ca3ff9a5db2c04f7bdc26539e704225c40
gerceboss <nilavprajapati2004@gmail.com>
2024-10-01 17:15:19 +0530

    added layout for opcode functions

---
+++ src/op_codes.rs
@@ -0,0 +1,18 @@
+use vm::registers::Register;
+
+pub fn op_add() {}
+pub fn op_and() {}
+pub fn op_not() {}
+pub fn op_ld() {}
+pub fn op_st() {}
+pub fn op_jmp() {}
+pub fn op_br() {}
+pub fn op_ldr() {}
+pub fn op_str() {}
+pub fn op_jsr() {}
+pub fn op_rti() {}
+pub fn op_ldi() {}
+pub fn op_sti() {}
+pub fn op_res() {}
+pub fn op_lea() {}
+pub fn op_trap() {}
