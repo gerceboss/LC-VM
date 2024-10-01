b3c4f8e588bc54e233e5245fd6a26ac91ba0af92
gerceboss <nilavprajapati2004@gmail.com>
2024-10-01 15:09:33 +0530

    added Opcodes module

--- src/lib.rs
+++ src/lib.rs
@@ -30,3 +30,43 @@ pub mod registers {
         }
     }
 }
+
+pub mod opcodes {
+    use std::ops::{Index, IndexMut};
+
+    // Now for instruction set , instructions supported are of 16 bits
+    // We need supported OP_CODES which are of 4 bits
+    #[allow(non_camel_case_types)]
+    enum OpCode {
+        OP_ADD,  // add
+        OP_AND,  // bitwise and
+        OP_NOT,  // bitwise not
+        OP_LD,   // load
+        OP_ST,   // store
+        OP_JMP,  // jump
+        OP_BR,   // branch
+        OP_LDR,  // load register
+        OP_STR,  // store register
+        OP_JSR,  // jump register
+        OP_RTI,  // unused
+        OP_LDI,  // load indirect
+        OP_STI,  // store indirect
+        OP_RES,  // reserved (unused)
+        OP_LEA,  // load effective address
+        OP_TRAP, // execute trap
+    }
+
+    // we need indexing for them as well
+    impl<T> Index<OpCode> for Vec<T> {
+        type Output = T;
+        fn index(&self, idx: OpCode) -> &Self::Output {
+            &self[idx as usize]
+        }
+    }
+
+    impl<T> IndexMut<OpCode> for Vec<T> {
+        fn index_mut(&mut self, idx: OpCode) -> &mut Self::Output {
+            &mut self[idx as usize]
+        }
+    }
+}
